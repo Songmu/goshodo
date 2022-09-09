bf66935347b945215f953c22cac0be0deb3c08b1
Songmu <y.songmu@gmail.com>
2022-09-09 22:36:02 +0900

    try to use suggestion comment

--- checkstyle.go
+++ checkstyle.go
@@ -4,6 +4,7 @@ import (
 	"encoding/xml"
 	"fmt"
 	"io"
+	"strings"
 )
 
 type CheckStyle struct {
@@ -34,12 +35,29 @@ func fprintCheckStyle(w io.Writer, lints []*Lint) error {
 			Name: l.File,
 		}
 		for _, m := range l.Result.Messages {
-			fix := ""
-			if m.After != "" {
-				fix = fmt.Sprintf("（→ %s）", m.After)
+			after := m.After
+			if after == "トル" {
+				after = ""
 			}
-			pre, match, post := l.Correspond(m)
-			msg := fmt.Sprintf("%s\n    %s~~%s~~%s%s", m.Message, pre, match, fix, post)
+			line := l.Line(m.From.Line)
+			lineLen := len([]rune(line))
+
+			pre, match, post := l.Correspond(m, lineLen)
+			var postMsg string
+			if !strings.Contains(match, "\n") {
+				pres := strings.Split(pre, "\n")
+				p := pres[len(pres)-1]
+				po := strings.Split(post, "\n")[0]
+				postMsg = fmt.Sprintf("```suggestsion\n%s%s%s\n```", p, after, po)
+			} else {
+				fix := ""
+				if m.After != "" {
+					fix = fmt.Sprintf("（→ %s）", m.After)
+				}
+				pre, match, post := l.Correspond(m, 10)
+				postMsg = fmt.Sprintf("    %s~~%s~~%s%s", pre, match, fix, post)
+			}
+			msg := fmt.Sprintf("%s\n%s", m.Message, postMsg)
 			f.Errors = append(f.Errors, CheckStyleError{
 				Line:     m.From.Line + 1,
 				Column:   m.From.Ch + 1,
